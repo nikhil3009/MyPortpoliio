fee7988b8f28e91906a2dc93689f6696518faa9f
nikhil3009 <38169100+nikhil3009@users.noreply.github.com>
2024-03-15 20:06:43 -0400

    Skills-section completed

---
+++ files/script.js
@@ -0,0 +1,18 @@
+/** @format */
+
+var typed = new Typed('.text', {
+	strings: ['Full-Stack Web Development.', 'Cloud Engineering.'],
+	typeSpeed: 100,
+	backSpeed: 100,
+	backDelay: 1000,
+	loop: true,
+});
+
+const toTop = document.querySelector('.top');
+window.addEventListener('scroll', () => {
+	if (window.pageYOffset > 100) {
+		toTop.classList.add('active');
+	} else {
+		toTop.classList.remove('active');
+	}
+});
